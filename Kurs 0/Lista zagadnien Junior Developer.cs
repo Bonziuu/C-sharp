bfc0fc878c5e9edfc43542bbdb72c0a4bfac9418
Bonziuu <99bonzoz99@gmail.com>
2023-04-03 15:47:12 +0200

    Dodanie treści

---
+++ Kurs 0/Lista zagadnien Junior Developer.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Typy zmiennych");
+Console.WriteLine("Typy wartościowe i referencyjne");
+Console.WriteLine("Pętle");
+Console.WriteLine("Warunki");
+Console.WriteLine("Tablice");
+Console.WriteLine("Listy");
+Console.WriteLine("Pętle");
+Console.WriteLine("Słowniki");
+Console.WriteLine("Inne kolekcje jak stos, kolejka");
+Console.WriteLine("Modyfikatory dostępu");
+Console.WriteLine("Klasy");
+Console.WriteLine("Propertiesy i konstruktory");
+Console.WriteLine("Klasy abstrakcyjne i interfejsy");
+Console.WriteLine("Programowanie obiektowe");
+Console.WriteLine("Paradygmaty programowania obiektowego (abstrakcja, enkapulsacja, dziedziczenia, polimorfizm)");
+Console.WriteLine("LINQ");
+Console.WriteLine("Tuples");
+Console.WriteLine("Przeładowanie metod i operatorów");
+Console.WriteLine("Extencion metody");
+Console.WriteLine("Podstawy programowania asynchronicznego async await");
+Console.WriteLine("Parametry funkcji out, ref");
+
+Console.WriteLine("https://www.youtube.com/watch?v=nU2egLdGmzs&ab_channel=seesharpify");
