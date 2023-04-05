2727d4b73be78c0ac45c7930f55d857ec92cdb9f
Bonziuu <99bonzoz99@gmail.com>
2023-04-05 17:17:08 +0200

    Kolejny dodany warunek if

--- 3. Instrukcje warunkowe/Class1.cs
+++ 3. Instrukcje warunkowe/Class1.cs
@@ -19,20 +19,30 @@ namespace _3._Instrukcje_warunkowe
 
             //Instrukcje warunkowe
 
-            Console.WriteLine("Instrukcje warunkowe\n-------------------------------------------\n\nif - jeżeli\n\nFunkcja ta jest używana w sposób:\n\nif(b) {\n    Console.WriteLine()\n      }\n\n\"JEŻELI B JEST TRUE (spełnione/prawdziwe) TO WYKONAJ KOMENDĘ WRITELINE\"\n");
-
-            if (b) {
+            Console.WriteLine("Instrukcje warunkowe\n-------------------------------------------\n\nif - jeżeli\n\nFunkcja ta jest używana w sposób:\n\nif(b) \n{\n    Console.WriteLine()\n}\n\n\"JEŻELI B JEST TRUE (spełnione/prawdziwe) TO WYKONAJ KOMENDĘ WRITELINE\"");
+            Console.WriteLine("-------------------------------------------\n");
+            Console.WriteLine("if (b == false) \n{\n    Console.WriteLine()\n}\n\n\"JEŻELI B JEST FALSE (niespełnione/fałszywe) TO NIE WYKONUJ WARUNKU IF\"");
+            Console.WriteLine("-------------------------------------------\n");
+            Console.WriteLine("if (a > 0) \n{\n    Console.WriteLine(\"a jest większe od 0\")\n}\nelse\n{\n    Console.WriteLine(\"a nie jest większe od 0\")\n}\n\n\"JEŻELI A JEST WIĘKSZE OD 0 TO WYKONAJ WRITELINE 'a jest większe od 0'\nA JEŚLI NIE TO WYKONAJ WRITELINE 'a nie jest większe od 0' IF\"");
+            Console.WriteLine("-------------------------------------------\n");
+            if (b)
+            {
                 Console.WriteLine("\nZmienna b jest prawdziwa");
             }
-
-            Console.WriteLine("\nif(b == false) {\n    Console.WriteLine()\n      }\n\n\"JEŻELI B JEST FALSE (niespełnione/fałszywe) TO NIE WYKONUJ WARUNKU\"\n");
 
             if (c == false)
             {
                 Console.WriteLine("Zmienna c jest fałszywa");
             }
             
-            
+            if (a > 0)
+            {
+                Console.WriteLine("a jest większe od 0");
+            }
+            else
+            {
+                Console.WriteLine("a nie jest większe od 0");
+            }
             Console.ReadKey();
         }
     }
