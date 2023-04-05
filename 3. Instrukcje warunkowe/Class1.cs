1d065db7b48609b80749280166aeffbd0b263c80
Bonziuu <99bonzoz99@gmail.com>
2023-04-05 16:59:38 +0200

    Opisanie instrukcji warunkowej if

--- 3. Instrukcje warunkowe/Class1.cs
+++ 3. Instrukcje warunkowe/Class1.cs
@@ -8,5 +8,32 @@ namespace _3._Instrukcje_warunkowe
 {
     internal class Class1
     {
+        static void Main(string[] args)
+        {
+            
+            //Deklaracje zmiennych
+            int a = 5;
+            bool b = true;
+            bool c = false;
+
+
+            //Instrukcje warunkowe
+
+            Console.WriteLine("Instrukcje warunkowe\n-------------------------------------------\n\nif - jeżeli\n\nFunkcja ta jest używana w sposób:\n\nif(b) {\n    Console.WriteLine()\n      }\n\n\"JEŻELI B JEST TRUE (spełnione/prawdziwe) TO WYKONAJ KOMENDĘ WRITELINE\"\n");
+
+            if (b) {
+                Console.WriteLine("\nZmienna b jest prawdziwa");
+            }
+
+            Console.WriteLine("\nif(b == false) {\n    Console.WriteLine()\n      }\n\n\"JEŻELI B JEST FALSE (niespełnione/fałszywe) TO NIE WYKONUJ WARUNKU\"\n");
+
+            if (c == false)
+            {
+                Console.WriteLine("Zmienna c jest fałszywa");
+            }
+            
+            
+            Console.ReadKey();
+        }
     }
 }
