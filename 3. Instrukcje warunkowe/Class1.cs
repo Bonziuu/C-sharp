30a92f670495b6b6d075ae9c552370502b805574
Bonziuu <99bonzoz99@gmail.com>
2023-04-05 17:52:20 +0200

    Poprawa if else

--- 3. Instrukcje warunkowe/Class1.cs
+++ 3. Instrukcje warunkowe/Class1.cs
@@ -19,12 +19,15 @@ namespace _3._Instrukcje_warunkowe
 
             //Instrukcje warunkowe
 
-            Console.WriteLine("Instrukcje warunkowe\n-------------------------------------------\n\nif - jeżeli\n\nFunkcja ta jest używana w sposób:\n\nif(b) \n{\n    Console.WriteLine()\n}\n\n\"JEŻELI B JEST TRUE (spełnione/prawdziwe) TO WYKONAJ KOMENDĘ WRITELINE\"");
+            Console.WriteLine("Instrukcje warunkowe\n-------------------------------------------\n\nif - jeżeli\nelse if - w przeciwnym razie, jeżeli\nelse - w przeciwnym razie\n\nFunkcja ta jest używana w sposób: (jeżeli b jest zmienną typu bool)\n\nif(b) \n{\n    Console.WriteLine();\n}\n\n\"JEŻELI B JEST TRUE (spełnione/prawdziwe) TO WYKONAJ KOMENDĘ WRITELINE\"");
             Console.WriteLine("-------------------------------------------\n");
-            Console.WriteLine("if (b == false) \n{\n    Console.WriteLine()\n}\n\n\"JEŻELI B JEST FALSE (niespełnione/fałszywe) TO NIE WYKONUJ WARUNKU IF\"");
+            Console.WriteLine("if (b == false) \n{\n    Console.WriteLine();\n}\n\n\"JEŻELI B JEST FALSE (niespełnione/fałszywe) TO NIE WYKONUJ WARUNKU IF\"");
             Console.WriteLine("-------------------------------------------\n");
-            Console.WriteLine("if (a > 0) \n{\n    Console.WriteLine(\"a jest większe od 0\")\n}\nelse\n{\n    Console.WriteLine(\"a nie jest większe od 0\")\n}\n\n\"JEŻELI A JEST WIĘKSZE OD 0 TO WYKONAJ WRITELINE 'a jest większe od 0'\nA JEŚLI NIE TO WYKONAJ WRITELINE 'a nie jest większe od 0' IF\"");
+            Console.WriteLine("if (a > 0) \n{\n    Console.WriteLine(\"a jest większe od 0\");\n}\nelse if (a == 0)\n{\n    Console.WriteLine(\"a jest równe 0 \");\n}\nelse\n{\n    Console.WriteLine(\"a jest mniejsze od 0\");\n}\n\n\"JEŻELI A JEST WIĘKSZE OD 0 TO WYKONAJ WRITELINE 'a jest większe od 0'\nW PRZECIWNYM RAZIE WYKONAJ WRITELINE 'a nie jest większe od 0'\"");
             Console.WriteLine("-------------------------------------------\n");
+
+            Console.ReadKey();
+
             if (b)
             {
                 Console.WriteLine("\nZmienna b jest prawdziwa");
@@ -39,9 +42,13 @@ namespace _3._Instrukcje_warunkowe
             {
                 Console.WriteLine("a jest większe od 0");
             }
+            else if (a == 0)
+            {
+                Console.WriteLine("a jest równe 0");
+            }
             else
             {
-                Console.WriteLine("a nie jest większe od 0");
+                Console.WriteLine("a jest mniejsze od 0");
             }
             Console.ReadKey();
         }
