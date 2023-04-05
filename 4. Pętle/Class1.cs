6dcb335f59f546762b9c15eea9c3d254efc414ee
Bonziuu <99bonzoz99@gmail.com>
2023-04-05 21:21:54 +0200

    Pętla while

--- 4. Pętle/Class1.cs
+++ 4. Pętle/Class1.cs
@@ -10,7 +10,42 @@ namespace _4._Pętle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            //Pętla WHILE - dopóki
+            int a = 0;
+            Console.WriteLine("Pętla while \n\nwhile (a < 10)\n{\n    Console.WriteLine(a);\n    a++;\n}\n\nRozwiązanie:\n");
+            while (a < 5)
+            {
+                Console.WriteLine(a);
+                a++;
+            }
+            //Wykonuj pętlę while dopóki a będzie mniejsze od 5, w której wypisz aktualną wartość zmiennej 'a', a następnie powiększ zmienną o 1 (inkrementacja)
+            
+            Console.WriteLine("\nDopóki zmienna 'a' będzie mniejsza od 10 to wykonaj pętlę, w której wypisz wartość zmiennej 'a', \na następnie zwiększ wartość zmiennej o 1 (inktementacja)");
+            Console.WriteLine("--------------------------------");
+
+
+            //Pętla FOR - dla
+            for (int b = 0; b < 5; b++)
+            {
+                Console.WriteLine(b);
+            }
+            //Zadeklarowana zmienna int b w nawiasie pętli for jest zadeklarowana WYŁĄCZNIE dla pętli i poza nia nie istnieje!
+            
+            Console.WriteLine();
+            int c = 0;
+            for (; c < 5; c++)
+            {
+                Console.WriteLine(c);
+            }
+            
+            Console.WriteLine();
+            int d;
+            for (d = 0; d < 5; d++)
+            {
+                Console.WriteLine(d);
+            }
+
+            Console.WriteLine();
             Console.ReadKey();
         }
     }
