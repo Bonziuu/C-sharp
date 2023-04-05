04680e661d448d240ac354e0aa367fd79da3955e
Bonziuu <99bonzoz99@gmail.com>
2023-04-05 20:17:46 +0200

    Nowy projekt

---
+++ 4. Pętle/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._Pętle
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.ReadKey();
+        }
+    }
+}
