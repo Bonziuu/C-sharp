68f74fd026eb84d985a8368511af9220cfaab715
Bonziuu <99bonzoz99@gmail.com>
2023-04-06 12:33:35 +0200

    Nowy projekt

---
+++ 5. Dodatkowe pojęcia teoretyczne/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._Dodatkowe_pojęcia_teoretyczne
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
