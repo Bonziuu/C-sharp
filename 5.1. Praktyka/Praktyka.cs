a0b2e9b29e28fb9c16a096ae7166fc6f530841e4
Bonziuu <99bonzoz99@gmail.com>
2023-04-06 16:31:04 +0200

    Nowy projekt praktyczny

---
+++ 5.1. Praktyka/Praktyka.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._1._Praktyka
+{
+    internal class Praktyka
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
