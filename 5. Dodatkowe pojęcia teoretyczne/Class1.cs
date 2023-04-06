ba2ed48b886244f15201f6e43c44f6a7ded7db28
Bonziuu <99bonzoz99@gmail.com>
2023-04-06 12:43:38 +0200

    Dodanie opisu komentarzy

--- 5. Dodatkowe pojęcia teoretyczne/Class1.cs
+++ 5. Dodatkowe pojęcia teoretyczne/Class1.cs
@@ -11,6 +11,20 @@ namespace _5._Dodatkowe_pojęcia_teoretyczne
         static void Main(string[] args)
         {
 
+            //Komentarze
+            Console.WriteLine("Komentarze\n");
+            Console.WriteLine("Komentarze można używać poprzez napisanie dwóch znaków '//':");
+            Console.WriteLine("//Komentarz");
+            Console.WriteLine("Jest to komentarz obejmujący tylko jedną linię kodu\n");
+            Console.WriteLine("Można także zakomentować jakąś część kodu poprzez użycie '/*' i '*/':");
+            Console.WriteLine("/*\n *Komentarz 1\n *Komentarz 2\n */");
+            Console.WriteLine("Taki komentarz obejmuje zbiór linii kodu zaczynając od momentu w którym zadelkarowany zostanie znak '/*'\ni będzie on komentarzem tak długo, aż nie zamkniemy zbioru znakiem '*/'");
+
+            /*
+             * Komentarz 1
+             * Komentarz 2
+             */
+            Console.ReadKey();
         }
     }
 }
