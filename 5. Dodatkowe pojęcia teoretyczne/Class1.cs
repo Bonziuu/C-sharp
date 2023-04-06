dd7544d57bde9eaf95c2138ad416a16f78086840
Bonziuu <99bonzoz99@gmail.com>
2023-04-06 12:57:30 +0200

    Skróty klawiszowe

--- 5. Dodatkowe pojęcia teoretyczne/Class1.cs
+++ 5. Dodatkowe pojęcia teoretyczne/Class1.cs
@@ -13,17 +13,29 @@ namespace _5._Dodatkowe_pojęcia_teoretyczne
 
             //Komentarze
             Console.WriteLine("Komentarze\n");
-            Console.WriteLine("Komentarze można używać poprzez napisanie dwóch znaków '//':");
+            Console.WriteLine("Komentarze można używać poprzez napisanie dwóch znaków '//':\n");
             Console.WriteLine("//Komentarz");
-            Console.WriteLine("Jest to komentarz obejmujący tylko jedną linię kodu\n");
-            Console.WriteLine("Można także zakomentować jakąś część kodu poprzez użycie '/*' i '*/':");
+            Console.WriteLine("\nJest to komentarz obejmujący tylko jedną linię kodu\n");
+            Console.WriteLine("Można także zakomentować jakąś część kodu poprzez użycie '/*' i '*/':\n");
             Console.WriteLine("/*\n *Komentarz 1\n *Komentarz 2\n */");
-            Console.WriteLine("Taki komentarz obejmuje zbiór linii kodu zaczynając od momentu w którym zadelkarowany zostanie znak '/*'\ni będzie on komentarzem tak długo, aż nie zamkniemy zbioru znakiem '*/'");
+            Console.WriteLine("\nTaki komentarz obejmuje zbiór linii kodu zaczynając od momentu w którym zadelkarowany zostanie znak '/*'\ni będzie on komentarzem tak długo, aż nie zamkniemy zbioru znakiem '*/'");
 
             /*
              * Komentarz 1
              * Komentarz 2
              */
+
+            Console.WriteLine("\n---------------------------------------------------------------------------------------------------------");
+
+            //Skróty klawiszowe i snippety
+            Console.WriteLine("Skróty klawiszowe i snippety\n");
+            Console.WriteLine("Skróty klawiszowe:");
+            Console.WriteLine("CTRL + S - zapisywanie programu");
+            Console.WriteLine("CTRL + K, a następnie CTRL + D - ustawienie linii kodu do pozycji (za dużo spacji itp)");
+            Console.WriteLine("CTRL + K, a następnie CTRL + C - zamiana zaznaczonego kodu w komentarz");
+            Console.WriteLine("CTRL + K, a następnie CTRL + U - przywrócenie zaznaczonego komentarza w kod");
+
+
             Console.ReadKey();
         }
     }
