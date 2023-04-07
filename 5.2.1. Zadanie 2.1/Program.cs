38f2d0e0a8d9bb9879d644da5b784609d6f46e1e
Bonziuu <99bonzoz99@gmail.com>
2023-04-07 14:35:03 +0200

    Program losowania liczb

--- 5.2.1. Zadanie 2.1/Program.cs
+++ 5.2.1. Zadanie 2.1/Program.cs
@@ -10,6 +10,41 @@ namespace _5._2._1._Zadanie_2._Rozwiązanie_1
     {
         static void Main(string[] args)
         {
+            //Gra w zgadywanie liczby wylosowanej przez komputer
+            //Komputer losuje liczbe od 1 - 10
+            //Użytkownik próbuje odgadnąć liczbę
+            //Jeżeli podana liczba jest mniejsza lub większa od wylosowanej to wyświetl komunikat
+            //Jeśli użytkownik trafi to pokaż komunikat wygranej
+
+            Random rnd = new Random();
+
+            int a = rnd.Next(1,11);
+            Console.WriteLine("Komputer wygenerował pewną liczbę w przedziale 1-10, zgadnij jaką!");
+            Console.WriteLine("Wpisz liczbę, którą podejrzewasz że komputer wylosował: ");
+            int b;
+            
+            do
+            {
+                b = int.Parse(Console.ReadLine());
+
+                if (b < a)
+                {
+                    Console.WriteLine("Liczba wygenerowana przez komputer jest większa");
+                    Console.WriteLine("Wpisz kolejną liczbę:");
+                }
+                else if (b > a)
+                {
+                    Console.WriteLine("Liczba wygenerowana przez komputer jest mniejsza");
+                    Console.WriteLine("Wpisz kolejną liczbę:");
+                }
+                else
+                {
+                    Console.WriteLine("Gratulacje! Odgadłeś liczbę!");
+                }
+            }
+            while (b != a);
+            // Pętla ma działać tak długo jak b będzie różne od a
+
             Console.ReadKey();
         }
     }
