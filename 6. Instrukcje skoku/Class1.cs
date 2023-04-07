796d092aa879a2cfc156b63c7e5f0319982a5beb
Bonziuu <99bonzoz99@gmail.com>
2023-04-07 15:39:33 +0200

    Nowy projekt

---
+++ 6. Instrukcje skoku/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._Instrukcje_skoku
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
