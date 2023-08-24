4ddf1cc898a76382a45e9ceb268fef096546db1e
Bonziuu <99bonzoz99@gmail.com>
2023-08-24 20:35:23 +0200

    Dodanie nowego projektu

---
+++ 10. Klasy i obiekty/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Klasy_i_obiekty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
