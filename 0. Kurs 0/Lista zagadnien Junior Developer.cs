cb4c8f66c8d9454d8848e09f7a1cf6efcb2fb167
Bonziuu <99bonzoz99@gmail.com>
2023-08-25 19:11:35 +0200

    Wprowadzono podstawowe informacje dotyczące klas i obiektów

--- 0. Kurs 0/Lista zagadnien Junior Developer.cs
+++ 0. Kurs 0/Lista zagadnien Junior Developer.cs
@@ -22,3 +22,5 @@ Console.WriteLine("Parametry funkcji out, ref");
 Console.WriteLine("Testy jednostkowe");
 Console.WriteLine("https://www.youtube.com/watch?v=nU2egLdGmzs&ab_channel=seesharpify");
 Console.WriteLine("https://www.youtube.com/watch?v=mSpCRUEUMUU&ab_channel=kierunekdotNET");
+Console.WriteLine("Roadmapa: \n" +
+    "https://www.youtube.com/watch?v=8tA--k8ExxI&ab_channel=DevMentors");
--- a10. Klasy i obiekty/Program.cs
+++ a10. Klasy i obiekty/Program.cs
@@ -10,7 +10,30 @@ namespace a10.Klasy_i_obiekty
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("--------------------------------------\nKlasy i obiekty\n--------------------------------------");
+            Console.WriteLine("\nTworzenie nowej klasy musi być umiejscowione w przestrzeni nazw 'namespace' \n" +
+                "i może byc utworzone w tym samym pliku .cs lub w nowej utworzonej klasie za pomocą prawego przycisku myszy \n" +
+                "na rozwiązaniu w 'Eksploratorze rozwiązań' -> Dodaj -> Nowy element -> Klasa C#\n" +
+                "\nPrzykładem nowej klasy może byc deklaracja 'class Nowaklasa{}''\n\n" +
+                "Jeśli mamy zadeklarowane jakieś zmienne w 'Nowaklasa' i chcielibyśmy z tych zmiennych korzystać w innej klasie \n" +
+                "np w Main to domyślnie nie jest to możliwe. W takim wypadku trzeba dodać przed zmiennymi słowo 'public' \n" +
+                "np: 'public string zmienna1;'\n\n\n" +
+                "Korzystanie z takiej zmiennej w innej klasie wyglądałoby w następujący sposób:\n\n" +
+                "Nowaklasa nazwa_obiektu = new Nowaklasa();\n" +
+                "nazwa_obiektu.zmienna1 = 10\n\nPrzykład:\n\n" +
+                "class Nowaklasa\r\n    {\r\n        public int zmienna1;\r\n    }\n" +
+                "\nstatic void Main(string[] args)\r\n    {\r\n        Nowaklasa nazwa_obiektu = new Nowaklasa();\n        nazwa_obiektu.zmienna1 = 10\r\n    }");
+            Console.WriteLine("\n'Nazwa_obiektu' to nazwa lokalna działająca tylko w obrębie metody w której została utworzona \n" +
+                "i takich obiektów można stworzyć więcej niż jeden.\n" +
+                "Podobnie robi się z metodami gdzie także używamy słowka public np 'public void Metoda{}' \n" +
+                "i wtedy utworzony w innej klasie obiekt można wywołać jako:\n" +
+                "nazwa_obiektu.");
             Console.ReadKey();
         }
+    }
+
+    class Nowaklasa
+    {
+        public int zmienna1;
     }
 }
